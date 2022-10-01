37dfa36dacfc6a9b5b0ff47c160e0e47ac3ed5d9
frobles03 <f.robles03@fromail.cl>
2022-09-30 21:29:06 -0300

    3 listo

--- tarea3.js
+++ tarea3.js
@@ -1,5 +1,7 @@
 const cars = require('./cars.json');
-
+/**
+ * 1
+ */
 let contador=0;
 cars.forEach(function(item) {
     if(item.year>2010){
@@ -13,7 +15,9 @@ console.log("cantidad de autos con años superior a 2010: " + contador);
 contador=0;
 
 
-
+/**
+ * 2
+ */
 cars.forEach(function(item) {
     if(item.color==="Red"){
     contador++;
@@ -21,5 +25,18 @@ cars.forEach(function(item) {
     }
 });
     console.log("cantidad de autos de color rojo es : " + contador);
+/**
+ * 3
+ */
+ cars.forEach(function(item) {
+    if(item.year>2010){
+        console.log(item.brand+"-"+item.model+" "+item.year);
+       
+    }
+    
+});
+
+
+
 
 //usar filter
